62f2894f98b110680cf4ba47c96060ab0e9dd451
AlcidesNeto32 <alcidesnda@gmail.com>
2026-08-11 14:45:31 -0300

    refactor: refactored some functions and the tight coupling was removed

--- model/service/task_service.py
+++ model/service/task_service.py
@@ -2,10 +2,11 @@ from repository.task_repository import TaskRepository
 from termcolor import colored
 from model.entity.task import Task
 
-repository = TaskRepository()
 class TaskService:  
-    
-    def row_to_task(self,sql=None,sql_list:list = None):
+    def __init__(self, repository:TaskRepository):
+        self.__repository = repository
+        
+    def row_to_task(self,sql=None,sql_result_list:list = None):
         if sql:
             for info in sql:
                 return Task(
@@ -18,7 +19,7 @@ class TaskService:
                 )
         else:
             task_list = []
-            for info in sql_list:
+            for info in sql_result_list:
                 task_list.append(
                     Task(
                         id = info[0],
@@ -33,24 +34,27 @@ class TaskService:
                 
     def update_task(self,task:Task):
         try:
-            repository.update(task=task)
+            self.__repository.update(task=task)
             return colored("Task updated!","green")
         except:
             return colored("Ops! A error happend!","red")
         
     def show_all_tasks(self):
-        return self.row_to_task(sql_list= repository.show_all())
+        return self.row_to_task(sql_result_list= self.__repository.show_all())
     
     def search_task_by_id(self,id:int):
-        return self.row_to_task(sql=repository.search_by_id(id))
-    
+        task = self.row_to_task(sql=self.__repository.search_by_id(id))
+        if task:
+            return task
+        else:
+            return colored("Task not found","red")
     def save_task(self,task:Task):
-        repository.save(task=task)
+        self.__repository.save(task=task)
         return colored("Task saved!","green")
     
     def delete_task(self,id:int):
         if self.search_task_by_id(id):
-            repository.delete(id)
+            self.__repository.delete(id)
             return colored("Task deleted!","green")
         else:
             return colored("Task does not exist!","red")
